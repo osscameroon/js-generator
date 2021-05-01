2fdb1e8c6aa337f10ff6eccc752fe5f3eb040677
Fanon Jupkwo <jupsfan@gmail.com>
2021-05-01 20:46:41 +0200

    Using StringBuilder instead of String

--- src/main/java/com/osscameroon/Main.java
+++ src/main/java/com/osscameroon/Main.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package com.osscameroon;
 
 import org.jsoup.Jsoup;
@@ -11,22 +14,35 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String sampleHtml = "" + "<!-- Button trigger modal -->\n"
-				+ "<button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\">\n"
-				+ "  Launch demo modal\n" + "</button>\n" + "\n" + "<!-- Modal -->\n"
-				+ "<div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">\n"
-				+ "  <div class=\"modal-dialog\" role=\"document\">\n" + "    <div class=\"modal-content\">\n"
-				+ "      <div class=\"modal-header\">\n"
-				+ "        <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>\n"
-				+ "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n"
-				+ "          <span aria-hidden=\"true\">&times;</span>\n" + "        </button>\n" + "      </div>\n"
-				+ "      <div class=\"modal-body\">\n" + "        ...\n" + "      </div>\n"
-				+ "      <div class=\"modal-footer\">\n"
-				+ "        <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>\n"
-				+ "        <button type=\"button\" class=\"btn btn-primary\">Save changes</button>\n" + "      </div>\n"
-				+ "    </div>\n" + "  </div>\n" + "</div>";
+		StringBuilder sampleHtml = new StringBuilder();
 
-		Element doc = Jsoup.parse(sampleHtml, "", Parser.xmlParser());
+		sampleHtml.append("<!-- Button trigger modal -->\n")
+					.append("<button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\">\n")
+					.append("  Launch demo modal\n")
+					.append("</button>\n")
+					.append("\n")
+					.append("<!-- Modal -->\n")
+					.append("<div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">\n")
+					.append("  <div class=\"modal-dialog\" role=\"document\">\n")
+					.append("    <div class=\"modal-content\">\n")
+					.append("      <div class=\"modal-header\">\n")
+					.append("        <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>\n")
+					.append("        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n")
+					.append("          <span aria-hidden=\"true\">&times;</span>\n")
+					.append("        </button>\n")
+					.append("      </div>\n")
+					.append("      <div class=\"modal-body\">\n")
+					.append("        ...\n")
+					.append("      </div>\n")
+					.append("      <div class=\"modal-footer\">\n")
+					.append("        <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>\n")
+					.append("        <button type=\"button\" class=\"btn btn-primary\">Save changes</button>\n")
+					.append("      </div>\n")
+					.append("    </div>\n")
+					.append("  </div>\n")
+					.append("</div>");
+
+		Element doc = Jsoup.parse(sampleHtml.toString(), "", Parser.xmlParser());
 		System.out.println(JsElementService.parseElement(doc));
 
 	}
