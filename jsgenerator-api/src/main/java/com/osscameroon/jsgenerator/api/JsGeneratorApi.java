662d0670df8c8c543e06688f98e95bc897ce0e66
Salathiel Genese <salathiel@genese.name>
2022-09-13 04:45:30 +0200

    feat: configure and test actuator and security

--- jsgenerator-api/src/main/java/com/osscameroon/jsgenerator/api/JsGeneratorApi.java
+++ jsgenerator-api/src/main/java/com/osscameroon/jsgenerator/api/JsGeneratorApi.java
@@ -1,13 +1,54 @@
 package com.osscameroon.jsgenerator.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.boot.SpringApplication.run;
+import static org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.toAnyEndpoint;
 
+@RestController
+@EnableWebSecurity
+@EnableMethodSecurity
 @SpringBootApplication
 public class JsGeneratorApi {
+    public final static String ACTUATOR_ROLE = "ACTUATOR";
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+        @Value("${management.endpoints.web.base-path:/actuator}") final String actuatorBasePath,
+        final HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf().disable()
+            .logout().disable()
+            .formLogin().disable()
+            .authorizeRequests()
+            .antMatchers(actuatorBasePath).permitAll()
+            .requestMatchers(toAnyEndpoint().excluding(HealthEndpoint.class).excludingLinks()).hasAnyRole(ACTUATOR_ROLE)
+            .and().httpBasic()
+            .and().build();
+    }
+
     public static void main(String[] args) {
         //noinspection resource
         run(JsGeneratorApi.class, args);
+    }
+
+    @GetMapping("hi")
+    @PreAuthorize("hasRole('HI')")
+    public String sayHi() {
+        return "Hi";
+    }
+
+    @GetMapping("nigga")
+    public String sayNigga() {
+        return "Nigga";
     }
 }
