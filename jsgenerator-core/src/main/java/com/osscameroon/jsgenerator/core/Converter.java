5adab23b07f7ecada859e962e268de9f066eb7d1
Salathiel Genese <salathiel@genese.name>
2022-10-17 00:21:16 +0200

    feat: propagate convert exceptions up the call chain

--- jsgenerator-core/src/main/java/com/osscameroon/jsgenerator/core/Converter.java
+++ jsgenerator-core/src/main/java/com/osscameroon/jsgenerator/core/Converter.java
@@ -3,16 +3,17 @@ package com.osscameroon.jsgenerator.core;
 import com.osscameroon.jsgenerator.core.internal.ConverterDefault;
 import lombok.NonNull;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 @FunctionalInterface
 public interface Converter {
-    default void convert(@NonNull final InputStream inputStream, @NonNull final OutputStream outputStream) {
+    default void convert(@NonNull final InputStream inputStream, @NonNull final OutputStream outputStream) throws IOException {
         convert(inputStream, outputStream, new Configuration());
     }
 
-    void convert(@NonNull final InputStream inputStream, @NonNull final OutputStream outputStream, @NonNull Configuration configuration);
+    void convert(@NonNull final InputStream inputStream, @NonNull final OutputStream outputStream, @NonNull Configuration configuration) throws IOException;
 
     static Converter of() {
         return new ConverterDefault();
