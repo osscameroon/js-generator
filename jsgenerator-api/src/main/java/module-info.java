9f9e2f821c5681a81d7dd44c63fd200daba95ba6
Salathiel Genese <salathiel@genese.name>
2022-10-17 07:53:33 +0200

    feat: add JPMS support to API module

---
+++ jsgenerator-api/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module com.osscameroon.jsgenerator.api {
+    opens com.osscameroon.jsgenerator.api to spring.beans;
+    opens com.osscameroon.jsgenerator.api.domain;
+    opens com.osscameroon.jsgenerator.api.rest to spring.beans;
+
+    requires com.osscameroon.jsgenerator.core;
+
+    requires spring.beans;
+    requires spring.boot.actuator;
+    requires spring.boot;
+    requires spring.boot.actuator.autoconfigure;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.security.config;
+    requires spring.security.web;
+    requires spring.web;
+
+    requires lombok;
+    requires org.slf4j;
+    requires java.validation;
+}
