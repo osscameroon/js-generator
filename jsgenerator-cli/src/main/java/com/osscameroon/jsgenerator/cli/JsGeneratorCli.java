5f3d0a2076b268bf7c8dbb33716df201e951548a
Salathiel Genese <salathiel@genese.name>
2022-10-17 06:55:19 +0200

    feat: make cli use core Spring Boot autoconfiguration

--- jsgenerator-cli/src/main/java/com/osscameroon/jsgenerator/cli/JsGeneratorCli.java
+++ jsgenerator-cli/src/main/java/com/osscameroon/jsgenerator/cli/JsGeneratorCli.java
@@ -5,14 +5,15 @@ import com.osscameroon.jsgenerator.core.Converter;
 import com.osscameroon.jsgenerator.core.OutputStreamResolver;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import picocli.CommandLine;
 
 import static java.lang.System.exit;
 import static org.springframework.boot.SpringApplication.run;
 
+@EnableAutoConfiguration
 @SpringBootConfiguration
-@SuppressWarnings("SpringFacetCodeInspection")
 public class JsGeneratorCli {
     public static void main(String[] args) {
         //noinspection resource
@@ -23,26 +24,6 @@ public class JsGeneratorCli {
     public CommandLineRunner picoCliRunner(final Command command) {
         final var commandLine = new CommandLine(command);
         return args -> exit(commandLine.execute(args));
-    }
-
-    @Bean
-    public OutputStreamResolver pathOutputStreamResolver() {
-        return OutputStreamResolver.ofPath();
-    }
-
-    @Bean
-    public OutputStreamResolver stdinOutputStreamResolver() {
-        return OutputStreamResolver.ofStdin();
-    }
-
-    @Bean
-    public OutputStreamResolver inlineOutputStreamResolver() {
-        return OutputStreamResolver.ofInline();
-    }
-
-    @Bean
-    public Converter converter() {
-        return Converter.of();
     }
 
     @Bean
