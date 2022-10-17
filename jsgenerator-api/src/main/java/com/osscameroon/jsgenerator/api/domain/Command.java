93fba369e7178e89f77ed3cabfafd9a84cb861a4
Salathiel Genese <salathiel@genese.name>
2022-10-17 07:42:19 +0200

    feat: allow for more configuration in API command

--- jsgenerator-api/src/main/java/com/osscameroon/jsgenerator/api/domain/Command.java
+++ jsgenerator-api/src/main/java/com/osscameroon/jsgenerator/api/domain/Command.java
@@ -1,11 +1,16 @@
 package com.osscameroon.jsgenerator.api.domain;
 
+import com.osscameroon.jsgenerator.core.BuiltinVariableNameStrategy;
+import com.osscameroon.jsgenerator.core.VariableDeclaration;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.osscameroon.jsgenerator.core.BuiltinVariableNameStrategy.TYPE_BASED;
+import static com.osscameroon.jsgenerator.core.VariableDeclaration.CONST;
 
 @Data
 public class Command {
@@ -14,4 +19,8 @@ public class Command {
     @Size(min = 1)
     private List<@NotNull String> inlineContents = new ArrayList<>();
     private String inlinePattern = "inline.{{ index }}{{ extension }}";
+    // NOTE: Configuration options down here
+    private String targetElementSelector = ":root > body";
+    private VariableDeclaration variableDeclaration = CONST;
+    private BuiltinVariableNameStrategy variableNameStrategy = TYPE_BASED;
 }
