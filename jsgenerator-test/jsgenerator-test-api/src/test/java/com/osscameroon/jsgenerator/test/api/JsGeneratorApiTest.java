d00cff5e631257f75073120de22fc9645d169a74
Salathiel Genese <salathiel@genese.name>
2022-10-26 19:14:20 +0200

    feat: test POST /convert

--- jsgenerator-test/jsgenerator-test-api/src/test/java/com/osscameroon/jsgenerator/test/api/JsGeneratorApiTest.java
+++ jsgenerator-test/jsgenerator-test-api/src/test/java/com/osscameroon/jsgenerator/test/api/JsGeneratorApiTest.java
@@ -1,33 +1,40 @@
 package com.osscameroon.jsgenerator.test.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.osscameroon.jsgenerator.api.JsGeneratorApi;
 import com.osscameroon.jsgenerator.api.rest.ConvertController;
+import com.osscameroon.jsgenerator.core.VariableDeclaration;
+import org.hamcrest.CustomMatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+import java.util.Map;
+
+import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@SpringBootTest(webEnvironment = MOCK, classes = {
-        DispatcherServletAutoConfiguration.class,
-        JsGeneratorApi.class,
-})
+@SpringBootTest(webEnvironment = MOCK)
 public class JsGeneratorApiTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
-    @Autowired
-    private ConvertController convertController;
+    private ObjectMapper objectMapper;
 
     private MockMvc mockMvc;
 
@@ -36,12 +43,7 @@ public class JsGeneratorApiTest {
         mockMvc = webAppContextSetup(webApplicationContext)
                 .apply(springSecurity())
                 .build();
-    }
-
-    @Test
-    public void contextProperlyConfigured() {
-        assertThat(mockMvc).isNotNull();
-        assertThat(convertController).isNotNull();
+        objectMapper = JsonMapper.builder().build();
     }
 
     @Test
@@ -75,5 +77,66 @@ public class JsGeneratorApiTest {
         mockMvc.perform(get("/actuator/mappings"))
                 .andExpect(status().isOk())
                 .andExpect(header().string("Content-Type", "application/vnd.spring-boot.actuator.v3+json"));
+    }
+
+    @ParameterizedTest
+    @EnumSource(VariableDeclaration.class)
+    public void convertInlineContent(
+            final VariableDeclaration variableDeclaration) throws Exception {
+        final var keyword = keyword(variableDeclaration);
+        final var extension = randomUUID().toString();
+        final var prefix = randomUUID().toString();
+        final var content = randomUUID().toString();
+
+        mockMvc.perform(post(ConvertController.MAPPING)
+                        .header(CONTENT_TYPE, APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(Map.of(
+                                "inlineContents", List.of("<div contenteditable>%s</div>".formatted(content)),
+                                "inlinePattern", "%s.{{ index }}{{ extension }}".formatted(prefix),
+                                "variableDeclaration", variableDeclaration,
+                                "extension", ".%s".formatted(extension)
+                        ))))
+                .andExpectAll(
+                        status().isOk(),
+                        header().string(CONTENT_TYPE, APPLICATION_JSON.toString()),
+                        jsonPath("$").isArray(),
+                        jsonPath("$.length()").value(1),
+                        jsonPath("$.[0].filename").value("%s.0.%s".formatted(prefix, extension)),
+                        jsonPath("$.[0].content").value(new Match(new String[]{
+                                "%s targetElement_000 = document.querySelector(`:root > body`);".formatted(keyword),
+                                "%s div_000 = document.createElement('div');".formatted(keyword),
+                                "div_000.setAttribute(`contenteditable`, `true`);",
+                                "%s text_000 = document.createTextNode(`%s`);".formatted(keyword, content),
+                                "div_000.appendChild(text_000);",
+                                "targetElement_000.appendChild(div_000);",
+                        }))
+                );
+    }
+
+    private static String keyword(final VariableDeclaration variableDeclaration) {
+        return variableDeclaration.name().toLowerCase();
+    }
+
+    public static class Application extends JsGeneratorApi {
+    }
+
+    private static final class Match extends CustomMatcher<String> {
+        private final String[] lines;
+
+        public Match(final String[] lines) {
+            super("jsjenerator-matcher");
+            this.lines = lines;
+        }
+
+        @Override
+        public boolean matches(Object actual) {
+            assertThat(((String) actual)
+                    .lines()
+                    .map(String::strip)
+                    .filter(line -> !line.isEmpty())
+                    .toArray(String[]::new)
+            ).containsExactly(lines);
+            return true;
+        }
     }
 }
--- jsgenerator-test/jsgenerator-test-api/src/test/java/module-info.java
+++ jsgenerator-test/jsgenerator-test-api/src/test/java/module-info.java
@@ -1,10 +1,12 @@
 module com.osscameroon.jsgenerator.test.api {
     requires com.osscameroon.jsgenerator.api;
+    requires com.osscameroon.jsgenerator.core;
 
+    requires com.fasterxml.jackson.databind;
+    requires org.hamcrest;
+    requires org.junit.jupiter.params;
     requires spring.beans;
-    requires spring.boot.autoconfigure;
     requires spring.boot.test;
-    requires spring.boot.test.autoconfigure;
     requires spring.security.test;
     requires spring.test;
     requires spring.web;
