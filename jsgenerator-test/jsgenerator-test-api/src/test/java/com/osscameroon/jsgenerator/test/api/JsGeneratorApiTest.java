162bce88aff8fbfd7f0cefaa1ffd5fd871a74419
Fanon Jupkwo <jupsfan@gmail.com>
2022-11-24 16:52:49 +0000

    fix. improve api tests

--- jsgenerator-test/jsgenerator-test-api/src/test/java/com/osscameroon/jsgenerator/test/api/JsGeneratorApiTest.java
+++ jsgenerator-test/jsgenerator-test-api/src/test/java/com/osscameroon/jsgenerator/test/api/JsGeneratorApiTest.java
@@ -9,7 +9,8 @@ import org.hamcrest.CustomMatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +25,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.osscameroon.jsgenerator.test.api.helper.MultipartResultMatcher.withMultipart;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -43,13 +45,44 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @SpringBootTest(webEnvironment = MOCK)
 public class JsGeneratorApiTest {
-    private static final Resource SAMPLE_OUTPUT = new ClassPathResource("htmlFilesOutput/sample.js");
+    private static final Resource SAMPLE_OUTPUT_QUERY_SELECTOR_ADDED = new ClassPathResource("htmlFilesOutput/querySelectorAdded/sample.js");
+
+    private static final Resource SAMPLE_OUTPUT_QUERY_SELECTOR_NOT_ADDED = new ClassPathResource("htmlFilesOutput/querySelectorNotAdded/sample.js");
     private static final Resource SAMPLE = new ClassPathResource("htmlFilesInput/sample.html");
     @Autowired
     private WebApplicationContext webApplicationContext;
     private ObjectMapper objectMapper;
 
     private MockMvc mockMvc;
+
+    private static String keyword(final VariableDeclaration variableDeclaration) {
+        return variableDeclaration.name().toLowerCase();
+    }
+
+    private static String[] toArray(String content) {
+        return content
+                .lines()
+                .map(String::strip)
+                .filter(line -> !line.isEmpty())
+                .toArray(String[]::new);
+    }
+
+    private static String fileContent(final Resource resource) throws IOException {
+        return new BufferedReader(new InputStreamReader(resource.getInputStream(), UTF_8))
+                .lines().collect(joining("\r\n"));
+    }
+
+    private static Stream<Arguments> provideVariableDeclarationsAndQuerySelectorAdded() {
+        return Stream.of(
+                Arguments.of(VariableDeclaration.LET, true),
+                Arguments.of(VariableDeclaration.LET, false),
+                Arguments.of(VariableDeclaration.VAR, true),
+                Arguments.of(VariableDeclaration.VAR, false),
+                Arguments.of(VariableDeclaration.CONST, true),
+                Arguments.of(VariableDeclaration.CONST, false)
+
+        );
+    }
 
     @BeforeEach
     public void beforeEach() {
@@ -94,67 +127,136 @@ public class JsGeneratorApiTest {
     }
 
     @ParameterizedTest
-    @EnumSource(VariableDeclaration.class)
-    public void convertInlineContent(final VariableDeclaration variableDeclaration) throws Exception {
+    @MethodSource("provideVariableDeclarationsAndQuerySelectorAdded")
+    public void convertInlineContent(final VariableDeclaration variableDeclaration, final boolean querySelectorAdded) throws Exception {
         final var keyword = keyword(variableDeclaration);
         final var extension = randomUUID().toString();
         final var prefix = randomUUID().toString();
         final var content = randomUUID().toString();
 
-        mockMvc.perform(post(ConvertController.MAPPING)
-                        .header(CONTENT_TYPE, APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(of(
-                                "contents", List.of("<div contenteditable>%s</div>".formatted(content)),
-                                "pattern", "%s.{{ index }}{{ extension }}".formatted(prefix),
-                                "variableDeclaration", variableDeclaration,
-                                "extension", ".%s".formatted(extension)
-                        ))))
-                .andExpectAll(
-                        status().isOk(),
-                        header().string(CONTENT_TYPE, APPLICATION_JSON_VALUE),
-                        jsonPath("$.status").value("SUCCESS"),
-                        jsonPath("$.content").isArray(),
-                        jsonPath("$.content.length()").value(1),
-                        jsonPath("$.content.[0].filename").value("%s.0.%s".formatted(prefix, extension)),
-                        jsonPath("$.content.[0].content").value(new Match(new String[]{
-                                "%s targetElement_000 = document.querySelector(`:root > body`);".formatted(keyword),
-                                "%s div_000 = document.createElement('div');".formatted(keyword),
-                                "div_000.setAttribute(`contenteditable`, `true`);",
-                                "%s text_000 = document.createTextNode(`%s`);".formatted(keyword, content),
-                                "div_000.appendChild(text_000);",
-                                "targetElement_000.appendChild(div_000);",
-                        })));
+        if (querySelectorAdded) {
+
+            mockMvc.perform(post(ConvertController.MAPPING)
+                            .header(CONTENT_TYPE, APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(of(
+                                    "contents", List.of("<div contenteditable>%s</div>".formatted(content)),
+                                    "pattern", "%s.{{ index }}{{ extension }}".formatted(prefix),
+                                    "variableDeclaration", variableDeclaration,
+                                    "extension", ".%s".formatted(extension),
+                                    "querySelectorAdded", true
+                            ))))
+                    .andExpectAll(
+                            status().isOk(),
+                            header().string(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+                            jsonPath("$.status").value("SUCCESS"),
+                            jsonPath("$.content").isArray(),
+                            jsonPath("$.content.length()").value(1),
+                            jsonPath("$.content.[0].filename").value("%s.0.%s".formatted(prefix, extension)),
+                            jsonPath("$.content.[0].content").value(new Match(new String[]{
+                                    "%s targetElement_000 = document.querySelector(`:root > body`);".formatted(keyword),
+                                    "%s div_000 = document.createElement('div');".formatted(keyword),
+                                    "div_000.setAttribute(`contenteditable`, `true`);",
+                                    "%s text_000 = document.createTextNode(`%s`);".formatted(keyword, content),
+                                    "div_000.appendChild(text_000);",
+                                    "targetElement_000.appendChild(div_000);",
+                            })));
+
+
+        } else {
+
+            mockMvc.perform(post(ConvertController.MAPPING)
+                            .header(CONTENT_TYPE, APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(of(
+                                    "contents", List.of("<div contenteditable>%s</div>".formatted(content)),
+                                    "pattern", "%s.{{ index }}{{ extension }}".formatted(prefix),
+                                    "variableDeclaration", variableDeclaration,
+                                    "extension", ".%s".formatted(extension),
+                                    "querySelectorAdded", false
+                            ))))
+                    .andExpectAll(
+                            status().isOk(),
+                            header().string(CONTENT_TYPE, APPLICATION_JSON_VALUE),
+                            jsonPath("$.status").value("SUCCESS"),
+                            jsonPath("$.content").isArray(),
+                            jsonPath("$.content.length()").value(1),
+                            jsonPath("$.content.[0].filename").value("%s.0.%s".formatted(prefix, extension)),
+                            jsonPath("$.content.[0].content").value(new Match(new String[]{
+
+                                    "%s div_000 = document.createElement('div');".formatted(keyword),
+                                    "div_000.setAttribute(`contenteditable`, `true`);",
+                                    "%s text_000 = document.createTextNode(`%s`);".formatted(keyword, content),
+                                    "div_000.appendChild(text_000);",
+
+                            })));
+
+
+        }
+
     }
 
     @ParameterizedTest
-    @EnumSource(VariableDeclaration.class)
-    public void convertUploadedFilesContent(final VariableDeclaration variableDeclaration) throws Exception {
+    @MethodSource("provideVariableDeclarationsAndQuerySelectorAdded")
+    public void convertUploadedFilesContent(final VariableDeclaration variableDeclaration, final boolean querySelectorAdded) throws Exception {
         final var keyword = keyword(variableDeclaration);
         final var extension = randomUUID().toString();
         final var prefix = randomUUID().toString();
 
-        mockMvc.perform(multipart(ConvertController.MAPPING + "/files")
-                        .file(new MockMultipartFile(
-                                "options", "config.json", APPLICATION_JSON_VALUE, objectMapper.writeValueAsString(of(
-                                "pattern", "%s.{{ index }}{{}}{{ extension }}".formatted(prefix),
-                                "variableDeclaration", variableDeclaration,
-                                "extension", ".%s".formatted(extension)
-                        )).getBytes(UTF_8)))
-                        .file(new MockMultipartFile(
-                                "files", SAMPLE.getFilename(), MULTIPART_FORM_DATA_VALUE, SAMPLE.getInputStream()))
-                        .file(new MockMultipartFile(
-                                "files", SAMPLE.getFilename(), MULTIPART_FORM_DATA_VALUE, SAMPLE.getInputStream())))
-                .andExpectAll(
-                        status().isOk(),
-                        withMultipart().size(2),
-                        withMultipart().nth(0).exists(),
-                        withMultipart().nth(1).exists(),
-                        withMultipart().nth(0)
-                                .map(JsGeneratorApiTest::toArray)
-                                .passContent(lines -> assertThat(lines).containsExactly(
-                                        toArray(fileContent(SAMPLE_OUTPUT).replaceAll("\\{\\{\s*keyword\s*}}", keyword)))),
-                        content().contentTypeCompatibleWith(MULTIPART_FORM_DATA),
-                        header().string(CONTENT_TYPE, matchesPattern("^%s;boundary=.*$".formatted(MULTIPART_FORM_DATA_VALUE))));
+        if (querySelectorAdded) {
+
+            mockMvc.perform(multipart(ConvertController.MAPPING + "/files")
+                            .file(new MockMultipartFile(
+                                    "options", "config.json", APPLICATION_JSON_VALUE, objectMapper.writeValueAsString(of(
+                                    "pattern", "%s.{{ index }}{{}}{{ extension }}".formatted(prefix),
+                                    "variableDeclaration", variableDeclaration,
+                                    "extension", ".%s".formatted(extension),
+                                    "querySelectorAdded", true
+                            )).getBytes(UTF_8)))
+                            .file(new MockMultipartFile(
+                                    "files", SAMPLE.getFilename(), MULTIPART_FORM_DATA_VALUE, SAMPLE.getInputStream()))
+                            .file(new MockMultipartFile(
+                                    "files", SAMPLE.getFilename(), MULTIPART_FORM_DATA_VALUE, SAMPLE.getInputStream())))
+                    .andExpectAll(
+                            status().isOk(),
+                            withMultipart().size(2),
+                            withMultipart().nth(0).exists(),
+                            withMultipart().nth(1).exists(),
+                            withMultipart().nth(0)
+                                    .map(JsGeneratorApiTest::toArray)
+                                    .passContent(lines -> assertThat(lines).containsExactly(
+                                            toArray(fileContent(SAMPLE_OUTPUT_QUERY_SELECTOR_ADDED).replaceAll("\\{\\{\s*keyword\s*}}", keyword)))),
+                            content().contentTypeCompatibleWith(MULTIPART_FORM_DATA),
+                            header().string(CONTENT_TYPE, matchesPattern("^%s;boundary=.*$".formatted(MULTIPART_FORM_DATA_VALUE))));
+
+
+        } else {
+
+            mockMvc.perform(multipart(ConvertController.MAPPING + "/files")
+                            .file(new MockMultipartFile(
+                                    "options", "config.json", APPLICATION_JSON_VALUE, objectMapper.writeValueAsString(of(
+                                    "pattern", "%s.{{ index }}{{}}{{ extension }}".formatted(prefix),
+                                    "variableDeclaration", variableDeclaration,
+                                    "extension", ".%s".formatted(extension),
+                                    "querySelectorAdded", false
+                            )).getBytes(UTF_8)))
+                            .file(new MockMultipartFile(
+                                    "files", SAMPLE.getFilename(), MULTIPART_FORM_DATA_VALUE, SAMPLE.getInputStream()))
+                            .file(new MockMultipartFile(
+                                    "files", SAMPLE.getFilename(), MULTIPART_FORM_DATA_VALUE, SAMPLE.getInputStream())))
+                    .andExpectAll(
+                            status().isOk(),
+                            withMultipart().size(2),
+                            withMultipart().nth(0).exists(),
+                            withMultipart().nth(1).exists(),
+                            withMultipart().nth(0)
+                                    .map(JsGeneratorApiTest::toArray)
+                                    .passContent(lines -> assertThat(lines).containsExactly(
+                                            toArray(fileContent(SAMPLE_OUTPUT_QUERY_SELECTOR_NOT_ADDED).replaceAll("\\{\\{\s*keyword\s*}}", keyword)))),
+                            content().contentTypeCompatibleWith(MULTIPART_FORM_DATA),
+                            header().string(CONTENT_TYPE, matchesPattern("^%s;boundary=.*$".formatted(MULTIPART_FORM_DATA_VALUE))));
+
+
+        }
+
     }
 
     @Test
@@ -165,23 +267,6 @@ public class JsGeneratorApiTest {
                         .header(CONTENT_TYPE, APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(of("extension", ".js"))))
                 .andExpectAll(status().isBadRequest());
-    }
-
-    private static String keyword(final VariableDeclaration variableDeclaration) {
-        return variableDeclaration.name().toLowerCase();
-    }
-
-    private static String[] toArray(String content) {
-        return content
-                .lines()
-                .map(String::strip)
-                .filter(line -> !line.isEmpty())
-                .toArray(String[]::new);
-    }
-
-    private static String fileContent(final Resource resource) throws IOException {
-        return new BufferedReader(new InputStreamReader(resource.getInputStream(), UTF_8))
-                .lines().collect(joining("\r\n"));
     }
 
     @SpringBootApplication(exclude = JsGeneratorApi.class)
