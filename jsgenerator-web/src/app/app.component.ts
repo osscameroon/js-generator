5a6102ef3719d018a1826d8d2624f237d069b724
Salathiel Genese <salathiel@genese.name>
2022-12-11 12:45:07 +0100

    feat: clean up generated content

--- jsgenerator-web/src/app/app.component.ts
+++ jsgenerator-web/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'jsgenerator-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'jsgenerator-web';
