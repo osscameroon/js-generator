f6ee9a2fe5ca40bdd7e34538c8700eea65dc24ca
Salathiel Genese <salathiel@genese.name>
2022-12-11 15:45:59 +0100

    feat: improve popup close with escape key

--- jsgenerator-web/src/app/components/popup.component.ts
+++ jsgenerator-web/src/app/components/popup.component.ts
@@ -5,6 +5,8 @@ import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
   templateUrl: './popup.component.html'
 })
 export class PopupComponent implements OnInit {
+  private static listenerRegistered = false;
+
   #opened = false;
 
   @Output()
@@ -26,11 +28,17 @@ export class PopupComponent implements OnInit {
   }
 
   ngOnInit() {
-    addEventListener('keyup', event => {
-      if ([event.key, event.code].includes('Escape') && this.opened) {
-        this.opened = false;
-      }
-    })
+    if (!PopupComponent.listenerRegistered) {
+      addEventListener('keyup', event => {
+        const IS_ESCAPE_KEY = [event.key, event.code].includes('Escape');
+        const MAY_BE_SHORTCUT = event.altKey || event.ctrlKey || event.metaKey || event.shiftKey || event.isComposing;
+
+        if (IS_ESCAPE_KEY && !MAY_BE_SHORTCUT && this.opened) {
+          this.opened = false;
+        }
+      });
+      PopupComponent.listenerRegistered = true;
+    }
   }
 
   open() {
