55a4ff53aa89a29e4114b35af0219f2b793f3380
Salathiel Genese <salathiel@genese.name>
2022-12-12 00:37:15 +0100

    feat: provide default configuration

--- jsgenerator-web/src/app/app.module.ts
+++ jsgenerator-web/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {ConfigurationComponent} from "./components/configuration.component";
 import {PopupComponent} from "./components/popup.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {CONFIGURATION} from "./domain/configuration";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,18 @@ import {ReactiveFormsModule} from "@angular/forms";
     AppRoutingModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: CONFIGURATION, useValue: {
+        targetElementSelector: ':root > body',
+        variableNameStrategy: 'TYPE_BASED',
+        extension: '.jsgenerator.js',
+        variableDeclaration: 'CONST',
+        popupOpened: false,
+      },
+    },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
