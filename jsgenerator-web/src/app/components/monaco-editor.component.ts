88c915ee75cfa4e60c813da2899ae3ac453d3c79
Salathiel Genese <salathiel@genese.name>
2022-12-12 04:48:54 +0100

    feat: detect resize and throttle dimension changes

--- jsgenerator-web/src/app/components/monaco-editor.component.ts
+++ jsgenerator-web/src/app/components/monaco-editor.component.ts
@@ -1,9 +1,10 @@
 import {MonacoEditorService} from "./monaco-editor.service";
-import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, ViewContainerRef} from "@angular/core";
 import type * as Monaco from "monaco-editor";
 import {editor} from "monaco-editor";
-import {first} from "rxjs";
+import {first, map, Observable, Subject, Subscription} from "rxjs";
 import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+import IDimension = editor.IDimension;
 
 declare const monaco: typeof Monaco;
 
@@ -11,32 +12,87 @@ declare const monaco: typeof Monaco;
   selector: 'jsgenerator-monaco-editor',
   templateUrl: './monaco-editor.component.html',
 })
-export class MonacoEditorComponent implements OnInit {
+export class MonacoEditorComponent implements OnInit, OnDestroy {
   #monacoEditorService: MonacoEditorService;
+  #dimension = new Subject<IDimension>();
+  #dimensionSubscription?: Subscription;
+  #viewContainerRef: ViewContainerRef;
+
   @ViewChild('monaco')
   monacoRef?: ElementRef<HTMLDivElement>;
+
   editor?: IStandaloneCodeEditor;
 
-  constructor(monacoEditorService: MonacoEditorService) {
+  constructor(monacoEditorService: MonacoEditorService,
+              viewContainerRef: ViewContainerRef) {
     this.#monacoEditorService = monacoEditorService;
+    this.#viewContainerRef = viewContainerRef;
   }
 
   ngOnInit() {
+    this.#dimensionSubscription = this.#dimension
+      .pipe(throttle<IDimension>(500))
+      .subscribe(console.log);
     const subscription = this.#monacoEditorService.load().pipe(first()).subscribe(() => {
       this.editor = monaco.editor.create(this.monacoRef?.nativeElement!, {
         autoClosingBrackets: 'always',
         automaticLayout: true,
         codeLens: true,
-        // dimension: {
-        //   width: 200,
-        //   height: 300,
-        // },
+        dimension: {
+          width: 251,
+          height: 397,
+        },
         language: 'html',
       });
       subscription.unsubscribe();
     });
   }
+
+  ngOnDestroy() {
+    this.#dimensionSubscription?.unsubscribe();
+    this.#dimension.complete();
+  }
+
+  @HostListener('window:resize')
+  onResizeEventHandler() {
+    const {width: w, height: h} = getComputedStyle(this.#viewContainerRef.element.nativeElement);
+    this.#dimension.next({width: parseInt(w), height: parseInt(h)});
+  }
 }
+
+const throttle = <T>(blackoutMilliseconds: number) => (source: Observable<T>): Observable<T> => {
+  const onTimeout = () => {
+    handle = setTimeout(onTimeout, blackoutMilliseconds) as any as number;
+
+    if (!collected) {
+      subject.next(cachedValue);
+      collected = true;
+    }
+  };
+  const subscription = source.subscribe({
+    error: reason => subject.error(reason),
+    next: value => {
+      cachedValue = value;
+      collected = false;
+
+      if (handle < 0) {
+        onTimeout();
+      }
+    },
+    complete: () => {
+      subscription.unsubscribe();
+      clearTimeout(handle);
+      subject.complete();
+    },
+  });
+  const subject = new Subject<T>();
+  let collected = false;
+  let cachedValue: T;
+  let handle = -1;
+
+  return subject;
+};
+
 /*
 
 <!DOCTYPE html>
