773bcbebfbd9a69beb062e80f4058a7689aa4cf1
Salathiel Genese <salathiel@genese.name>
2022-12-12 03:03:05 +0100

    feat: scaffold an actual Monaco Editor

--- jsgenerator-web/src/app/components/monaco-editor.component.ts
+++ jsgenerator-web/src/app/components/monaco-editor.component.ts
@@ -1,12 +1,38 @@
 import {MonacoEditorService} from "./monaco-editor.service";
-import {Component} from "@angular/core";
+import {Component, ElementRef, HostBinding, OnInit, ViewChild} from "@angular/core";
+import type * as Monaco from "monaco-editor";
+import {first} from "rxjs";
+import {editor} from "monaco-editor";
+import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+
+declare const monaco: typeof Monaco;
 
 @Component({
   selector: 'jsgenerator-monaco-editor',
   templateUrl: './monaco-editor.component.html',
 })
-export class MonacoEditorComponent {
-  constructor(private readonly monacoEditorService: MonacoEditorService) {
-    monacoEditorService.load().subscribe(console.log);
+export class MonacoEditorComponent implements OnInit {
+  #monacoEditorService: MonacoEditorService;
+  @ViewChild('monaco')
+  monacoRef?: ElementRef<HTMLDivElement>;
+  editor?: IStandaloneCodeEditor;
+
+  constructor(monacoEditorService: MonacoEditorService) {
+    this.#monacoEditorService = monacoEditorService;
+  }
+
+  ngOnInit() {
+    this.#monacoEditorService.load().pipe(first()).subscribe(() => {
+      this.editor = monaco.editor.create(this.monacoRef?.nativeElement!, {
+        autoClosingBrackets: 'always',
+        automaticLayout: true,
+        codeLens: true,
+        dimension: {
+          width: 200,
+          height: 300,
+        },
+        language: 'html',
+      });
+    });
   }
 }
