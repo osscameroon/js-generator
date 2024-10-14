833dbead6cb112f54281ec38e944daf5f69776bb
Fanon Jupkwo <jupsfan@gmail.com>
2024-10-14 22:49:30 +0100

    chore: #272 add junit-pioneer dependency to test modules (#274)

--- jsgenerator-test/jsgenerator-test-desktop/src/test/java/module-info.java
+++ jsgenerator-test/jsgenerator-test-desktop/src/test/java/module-info.java
@@ -2,4 +2,7 @@ module com.osscameroon.jsgenerator.test.desktop {
     exports com.osscameroon.jsgenerator.test.desktop;
     opens com.osscameroon.jsgenerator.test.desktop;
 
+    requires com.osscameroon.jsgenerator.core;
+    requires org.junitpioneer;
+
 }
