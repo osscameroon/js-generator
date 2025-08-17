e71a54e66b64305e774feb05faca470ec5da5c40
Fanon Jupkwo <jupsfan@gmail.com>
2025-08-17 11:57:21 +0100

    Add #!/bin/bash insctruction

--- .github/workflows/installMaven4.0.0-rc-4.sh
+++ .github/workflows/installMaven4.0.0-rc-4.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -sL https://archive.apache.org/dist/maven/maven-4/4.0.0-rc-4/binaries/apache-maven-4.0.0-rc-4-bin.tar.gz | tar xz
 sudo mv apache-maven-4.0.0-rc-4 /opt/maven
 echo "M2_HOME=/opt/maven" >> $GITHUB_ENV
